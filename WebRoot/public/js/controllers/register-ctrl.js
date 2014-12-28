e6cb9bf1a4ef79fc430e58875081e771e75441d7
yinliang489233 <478071633@qq.com>
2014-12-29 02:14:56 +0800

    goods

--- WebRoot/public/js/controllers/register-ctrl.js
+++ WebRoot/public/js/controllers/register-ctrl.js
@@ -1,3 +1,80 @@
+<<<<<<< HEAD
+APP.controller("registerCtrl", function($scope, $http, registerService) {
+
+	var user = $scope.user = {
+		username : '',
+		password : ''
+	};
+
+	$scope.$on("register", function(event, data) {
+		user.username = "";
+		user.password = "";
+		$("#repassword").val("");
+		alert(data);
+	});
+
+	var ischeck = false;
+	$("#username").blur(function() {
+
+		registerService.checkName($scope, $scope.user.username);
+		$scope.$on("checkName", function(event, data) {
+			if (data.length > 0) {
+				ischeck = true;
+				$("#name_msg").html("错误 : 该帐号名已被注册!!");
+			}else{
+				ischeck = false;
+				$("#name_msg").html("");
+			}
+		});
+	});
+
+	$scope.register = function() {
+		
+		
+		
+		
+
+		var repassword = $("#repassword").val();
+		var remember = $("#remember").is(':checked');
+		
+		if (ischeck) {
+			return;
+		}
+
+		if (user.username.length > 20 || user.username.length < 8) {
+			$("#name_msg").html("错误 : 账号应该为8-20位的数字或字母!");
+
+			return;
+		}
+		$("#name_msg").html('');
+
+		if (!/^(\d|[a-zA-Z])+$/.test(user.username)) {
+			$("#name_msg").html("错误 : 账号应该为8-20位的数字或字母!");
+			return;
+		}
+		$("#name_msg").html('');
+
+		if (user.password == '') {
+			$("#pwd_msg").html("提示 : 请输入密码!");
+			return;
+		}
+		$("#pwd_msg").html('');
+		if (user.password != repassword) {
+			$("#pwd_msg").html("提示 : 密码不一致!");
+			return;
+		}
+		$("#pwd_msg").html('');
+
+
+		if (!remember) {
+			return;
+		}
+		registerService.register($scope, $scope.user);
+
+	};
+
+});
+=======
 APP.controller("registerCtrl", function($scope, $http, registerService) {
 
 	var user = $scope.user = {
@@ -70,3 +147,4 @@ APP.controller("registerCtrl", function($scope, $http, registerService) {
 	};
 
 });
+>>>>>>> 1cb62d82052ebea8f040bb23d9e1e35f4ef68d8a
