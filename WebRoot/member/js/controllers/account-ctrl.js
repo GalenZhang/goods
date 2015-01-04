15d78294c3fe60a0994841ce4bf2b031bcfeceda
ray <51447zhg@163.com>
2015-01-04 12:18:27 +0800

    remove useless code.

--- WebRoot/member/js/controllers/account-ctrl.js
+++ WebRoot/member/js/controllers/account-ctrl.js
@@ -32,8 +32,6 @@ APP.controller("accountCtrl", function($rootScope, $scope, accountService, membe
 			return;
 		}
 
-		debugger;
-		
 		accountService.saveUserInfo($scope, account);
 		$scope.$on("saveUserInfo", function(event, data) {
 			alert(data.desc);
@@ -41,35 +39,3 @@ APP.controller("accountCtrl", function($rootScope, $scope, accountService, membe
 	};
 
 });
-function checkSubmitEmail() {
-	if ($("#email").val() == "") {
-		$("#confirmMsg").html("<font color='red'>邮箱地址不能为空！</font>");
-
-		$("#email").focus();
-		return false;
-	}
-	if (!$("#email")
-			.val()
-			.match(
-					/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/)) {
-
-		$("#confirmMsg").html("<font color='red'>邮箱格式不正确！请重新输入！</font>");
-		$("#email").focus();
-		return false;
-	}
-	return true;
-}
-function checkSubmitMobil() {
-	if ($("#phone").val() == "") {
-		 $("#moileMsg").html("<font color='red'>手机号码不能为空！</font>");
-		$("#phone").focus();
-		return false;
-	}
-
-	if (!$("#phone").val().match(/^(?:13\d|15\d|18\d)\d{5}(\d{3}|\*{3})$/)) {
-		$("#moileMsg").html("<font color='red'>手机号码格式不正确！请重新输入！</font>");
-		$("#phone").focus();
-		return false;
-	}
-	return true;
-}
